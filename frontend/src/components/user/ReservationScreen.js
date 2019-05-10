e6173fae9ac4acd81f914b8df992bced9ef3b718
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-05-10 06:54:47 -0500

    Reservation screen

---
+++ frontend/src/components/user/ReservationScreen.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+
+export default class ReservationScreen extends Component{
+    render() {
+        // TO-DO: replace null with HTML design
+        return(null)
+    }
+}

babaa5b4af206e9059f8d9e79bec172a0c4da3f0
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-05-13 12:09:29 -0500

    Changes on id model

--- config/mongoose/reservation.js
+++ config/mongoose/reservation.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 let entrySchema = mongoose.Schema({
     TABLE_ID: String,
-    SEAT_ID: String,
+    SEAT_ID: Number,
     USER_ID: String,
     STATUS: String,
     UNTIL: Number
